50c1dba92fab73c25747f367dcc47f00c7f8ede4
gaoyangy <13373935140@163.com>
2017-06-20 13:12:47 +0800

    morecode

--- config/index.js
+++ config/index.js
@@ -8,8 +8,8 @@ var api = {
 module.exports = {
     build: {
         env: require('./prod.env'),
-        index: path.resolve(__dirname, '../dist/index.html'),
-        assetsRoot: path.resolve(__dirname, '../dist'),
+        index: path.resolve(__dirname, '../../dist/index.html'),
+        assetsRoot: path.resolve(__dirname, '../../dist'),
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
         productionSourceMap: true,
