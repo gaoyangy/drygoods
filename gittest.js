574d5ac3c7414c71621c4d9783fbb90035e0eeee
gaoyangy <13373935140@163.com>
2017-06-26 17:26:21 +0800

    code

--- gittest.js
+++ gittest.js
@@ -1,17 +1,25 @@
 var http = require('http')
   , exec = require('exec')
+  , config= require('config/index.js')
 
 const PORT = 9988
   , PATH = '/usr/local/nginx/drygoods/'
 
 var deployServer = http.createServer(function(request, response) {
   if (request.url.search(/gitpull\/?$/i) > 0) {
-
+    if(config.isNpm){
+      var commands=[
+        'cd ' + PATH,
+        'git pull',
+        'npm install',
+        'npm run build'
+      ]
+    }else{
     var commands = [
       'cd ' + PATH,
       'git pull'
-    ].join(' && ')
-
+      ].join(' && ')
+    }
     exec(commands, function(err, out, code) {
       if (err instanceof Error) {
         response.writeHead(500)
