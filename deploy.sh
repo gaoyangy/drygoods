c08563283b16d2d167c33acb16c60c4f8df271a9
gaoyangy <13373935140@163.com>
2017-06-26 15:40:30 +0800

    all

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
  
-WEB_PATH='../usr/local/drygoods'
+WEB_PATH='/usr/local/drygoods'
  
 echo "Start deployment"
 cd $WEB_PATH
 echo "pulling source code..."
-git reset --hard origin/master
-git clean -f
 git pull
-git checkout master
 echo "changing permissions..."
-chown -R 777 $WEB_PATH
 echo "Finished."
