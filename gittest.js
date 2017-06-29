8d9b1a2aefc0402cccc20ef80a08e3e8a8a45379
gaoyangy <13373935140@163.com>
2017-06-29 14:33:02 +0800

    test

--- gittest.js
+++ gittest.js
@@ -1,48 +1,34 @@
 var http = require('http'),
     exec = require('exec'),
-    config = require('./config/index.js')
 
 const PORT = 9988,
-    PATH = '../drygoods/'
+    PATH = '/usr/local/nginx/drygoods/'
 
 var deployServer = http.createServer(function(request, response) {
     if (request.url.search(/gitpull\/?$/i) > 0) {
-        if (config.isNpm) {
-            var commands = [
-                'cd ' + PATH,
-                'git pull',
-                'npm install',
-                'npm run build' //如果本地没有编译而且有安装过其他插件
-            ]
-        }
-        if (config.isProduce) {
+        var commands = [
             'cd ' + PATH,
             'git pull',
-            'npm run build',
-            'cp -rf dist/* /www' //发布线上
-        } else {
-            var commands = [
-                'cd ' + PATH,
-                'git pull',
-                'npm run build'
-            ].join(' && ')
-        }
+            'npm run build'
+        ].join(' && ')
         exec(commands, function(err, out, code) {
             if (err instanceof Error) {
                 response.writeHead(500)
-                response.end('Server Internal Error.'+new Date())
+                response.end('Server Internal Error.')
                 throw err
             }
             process.stderr.write(err)
             process.stdout.write(out)
             response.writeHead(200)
-            response.end('Deploy Done.'+new Date())
+            response.end('Deploy Done.')
 
         })
 
     } else {
+
         response.writeHead(404)
         response.end('Not Found.' + new Date());
+
     }
 })
 
